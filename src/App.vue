6edd9c48ce3bb640e83254bb6374df1a58516d47
jdhaisne <julien.dhaisne@gmail.com>
2023-05-05 15:22:19 +0200

    try witouht /imgs

--- src/App.vue
+++ src/App.vue
@@ -9,28 +9,28 @@ import JPortfolio from "./components/JPortfolio.vue";
     :portfolio-list="[
       {
         title: 'kanban',
-        img: './src/assets/imgs/kanbanScreen.png',
+        img: '/src/assets/imgs/kanbanScreen.png',
         desc: 'A Simple kanban demonstration from front end mentor',
         vercelLink: 'https://kanban-task-management-five.vercel.app/',
         gitLink: 'https://github.com/jdhaisne/kanban-task-management',
       },
       {
         title: 'fylo data storage',
-        img: './assets/imgs/fyloScreen.png',
+        img: '/src/assets/imgs/fyloScreen.png',
         desc: 'A Simple animation from front end mentor',
         vercelLink: 'https://fylo-data-storage-component-inky-beta.vercel.app/',
         gitLink: 'https://github.com/jdhaisne/fylo-data-storage-component',
       },
       {
         title: 'social proof section',
-        img: './assets/imgs/socialScreen.png',
+        img: '/src/assets/imgs/socialScreen.png',
         desc: 'A Simple example of social proof from front end mentor',
         vercelLink: 'https://social-proof-section-master-lilac.vercel.app',
         gitLink: 'https://github.com/jdhaisne/social-proof-section-master',
       },
       {
         title: 'profil card',
-        img: '/src/assets/imgs/cardScreen.png',
+        img: '/src/assets/cardScreen.png',
         desc: 'A Simple profil card from front end mentor',
         vercelLink: 'https://profile-card-component-j5fwc0nuv.vercel.app/',
         gitLink: 'https://github.com/jdhaisne/profile-card-component',
